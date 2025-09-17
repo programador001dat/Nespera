4a2746994245ffbb524b6d9a07c2a03d8ee727fd
programador001dat <155966937+programador001dat@users.noreply.github.com>
2025-09-17 02:30:37 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require("express");  // Importando o modulo para dentro do codigo
+const path = require("path"); // Modulo de manipulação de caminhos
+const bodyParser = require("body-parser"); // Modulo para receber as requisições de um POST. etc.
 
-const router = express.Router();
+const router = express.Router(); // Criando uma estancia do roteamento com express 5.1.0
 
+// Configuração de Middleware, para cada GET retorne o IP da requisição.
 const simpleMiddleware = (req, res, next) => {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
     console.log(`=> requisição de ${ip}`);
     next();
 }
-router.use(simpleMiddleware);
+router.use(simpleMiddleware); // Use dentro das rotas
 
+// Criando uma rota
 router.get("/", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type","text/html");
-    res.sendFile(path.join(__dirname, "../views", "article.html"));
+    res.statusCode = 200; // Codigo de sucesso
+    res.setHeader("Content-Type","text/html"); // Tipo de cabeçalho HTML
+    res.sendFile(path.join(__dirname, "../views", "article.html")); // renderizar o que tem dentro da pasta estatica views
 });
 
 
-module.exports = router;
+
+module.exports = router;  // Exporta o codigo para outro arquivo.

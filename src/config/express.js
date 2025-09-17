2bcd66010b4846081e6c4bc6142f398a2342b66a
programador001dat <155966937+programador001dat@users.noreply.github.com>
2025-09-17 02:27:20 -0300

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require("express");  // Importando o modulo para dentro do codigo
 const path = require("path");
 const bodyParser = require("body-parser");
 
-const app = express();
+const app = express();  // Criando um estancia do servidor
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));  // Configurando o servidor para servir pasta estaticas
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-module.exports = app;
+
+module.exports = app;  // Exportando para usar em outro codigo.
